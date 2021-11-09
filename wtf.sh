bc1ee43c60a7c9ca3e520be26f9a98e7a2ff3f36
LeATeP <leateps@gmail.com>
2021-11-09 18:23:26 +0400

    add color changing bash code example

--- wtf.sh
+++ wtf.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 
-q1() {
-for i in *; do 
-   echo "$i"; done
-}
 
-q1 | nl | lolcat
+
+for i in $(seq 30 37); do
+	printf "\e[${i}m%s\n" "hello";  
+	done			
+# '\e' stand for color, '[' for value 'm' close
+
+	
+
+
+# q1() {
+# for i in *; do 
+#   echo "$i"; done
+#}
+#
+#q1 | nl | lolcat
