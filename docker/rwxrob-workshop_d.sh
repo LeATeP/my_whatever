90953845774333fd59df200f09e7456adf8e83c5
LeATeP <leateps@gmail.com>
2021-11-13 19:35:17 +0400

    minor improvement of code

--- docker/rwxrob-workshop_d.sh
+++ docker/rwxrob-workshop_d.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-name=$(docker_name_gen.py workshop)
+name=$(./docker_name_gen.py workshop)
 sudo docker run -it \
     -v "/home/leatep/Desktop/code/docker/$name:/home" \
+    -v "/var/run/docker.sock:/var/run/docker.sock" \
     -h "${name}" \
     --name "${name}" \
     rwxrob/workspace
