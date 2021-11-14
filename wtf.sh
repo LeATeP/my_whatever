e4ac416e934749fb8e6b9f85173ec075a4b34171
LeATeP <leateps@gmail.com>
2021-11-14 04:15:14 +0400

    add more specific and delailed code about bash colors

--- wtf.sh
+++ wtf.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 
-
-for i in $(seq 30 37); do
-	printf "\e[${i}m%s\n" "hello";  
-	done			
-# '\e' stand for color, '[' for value 'm' close
-
-	
-
+# 256 colors
+for i in $(seq 0 255); do
+	printf "\e[1;38;5;${i}m%s" "hello $i"
+done	# '\e[' initiate.. 1 for font style... 
+		# '38' for color mode.. 
+		# '5' for 255 / 2 for R;G;B
+		# '%s' for display.. + '\n'
+		
 
 # q1() {
 # for i in *; do 
