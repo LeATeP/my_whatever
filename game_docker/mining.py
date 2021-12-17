356e60fbd89d243178916e1774e64bcaa79c201d
LeATeP <leateps@gmail.com>
2021-12-17 18:24:21 +0400

    finishing refactoring initial code for mining

--- game_docker/mining.py
+++ game_docker/mining.py
@@ -6,71 +6,68 @@ from time import sleep
 
 env = environ.get
 path.append(env("GAME")) # my_whatever/game_docker
-from hub.wrap import init_conn
+from sql.sql_query import psql
 from colors import fg_rgb
-    
+
 class mining:
     def __init__(self) -> None:
-        pass
+        self.worker_ids = []
+        # self.user_config = {}
+        # self.tick_time = 1 if self.user_config['-t'] is None else self.user_config['-t']
     
-    def start_work(self, cmd):
+    def start_work(self):
         try:
+            cmd = psql().cmd
             data = cmd('''select * from worker
                        where working = false limit 1;''')
-            if data:
-                key = self.ret_key(data)
-                result = cmd(f'''update worker
-                    set working = true
-                    where id = {key}
-                    returning working;''')
-                result_confirmation = self.ret_key(result)
-                
-                ###
-                
-                try:
-                    if result_confirmation:
-                        while True:
-                            sleep(1)
-                            drop = self.gen_drop()
-                            drop = {n: drop[n] for n in drop if drop[n] is not None}
-                            if not drop:
-                                continue
-                            for n in drop:
-                                command = f'''update items
-                                set amount = amount + {drop[n]}
-                                where name = '{n}'
-                                returning amount;'''
-                                x = cmd(command)
-                        
-                except Exception:
-                    pass
-                
-                ###
             
-        except Exception:
-            pass
+            # if no worker are available then break
+            if not data:
+                raise Exception('no worker are Free')
+            
+            # change worker status on True
+            for worker in data.values():
+                id = worker['id']
+                cmd(f'''update worker set working = true
+                        where id = {id} returning working;''')
+                self.worker_ids.append(id) # add worker to pull
+                
+            
+            good = True    
+            while good:
+                sleep(0.1)
+                drop = self.gen_drop()
+                for item in drop:
+                    amount = cmd(f'''update items
+                    set amount = amount + {drop[item]}
+                    where name = '{item}' returning amount;''')[0]['amount']
+                    s = f"{item}: {amount}"
+                    print(fg_rgb(s))
+
+                
+                
+                
+        except (Exception) as error:
+            print(error.args)
         finally:
-            print('finally', str(key))
-            result = cmd(f'''update worker
-                    set working = false
-                    where id = {key}
-                    returning working;''')
+            for id in self.worker_ids:
+                result = cmd(f'''update worker
+                        set working = false
+                        where id = {id}
+                        returning working;''')
         
     def gen_drop(self):
         drop =  {'Rock': randint(1,2) if randint(1, 10) > 5 else None,
                 'Energy rock': 1 if randint(1, 10) > 8 else None,
                 'Iron ore': 1 if randint(1, 100) > 95 else None}
 
+        drop = {n: drop[n] for n in drop if drop[n] is not None}
         return drop
         
-    
-    def ret_key(self, data):
-        key = [n for n in data][0]
-        return key
 
 
 if __name__ == "__main__":
-    init_conn(mining().start_work)
+    mining().start_work()
         
 
 
