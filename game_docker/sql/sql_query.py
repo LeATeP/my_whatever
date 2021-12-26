cddcf5a6378b398f2d9e910577bd110e0c2d892f
LeATeP <leateps@gmail.com>
2021-12-26 20:51:30 +0400

    something

--- game_docker/sql/sql_query.py
+++ game_docker/sql/sql_query.py
@@ -5,33 +5,29 @@ from os import environ
 
 env = environ.get
 
+
 class psql:
     def __init__(self) -> None:
         self.conn = connect(host=str(env("POSTGRES_HOST")), 
                            database=str(env("POSTGRES_DB")), 
                            user=str(env("POSTGRES_USER")), 
                            password=str(env("POSTGRES_PASSWORD")))
+        self.conn.set_session(autocommit=True)
         self.curs = self.conn.cursor()
         
     def exec(self, cmd):
         try:
             self.curs.execute(cmd)
-    
         except (Exception, DatabaseError) as error:
             print(error.args)
- 
-    def commit(self):
-        self.conn.commit()
         
         
     def fetch_dict(self):       
         fetch = self.curs.fetchall()
         row_name = [n[0] for n in self.curs.description]
-        
         data = {}
         for i in range(len(fetch)):
             data[i] = dict(zip(row_name, fetch[i]))
-        
         return data
         
         
