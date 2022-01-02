2220b8c81214a281c0f18b56f757a58db7d8894a
LeATeP <leateps@gmail.com>
2022-01-02 08:58:03 +0400

    small fixes

--- game_docker/mining.py
+++ game_docker/mining.py
@@ -7,38 +7,37 @@ from time import sleep
 env = environ.get
 path.append(env("GAME")) # my_whatever/game_docker
 from sql.sql_query import psql
-from colors import fg_rgb
+from utils.colors import fg_rgb
 
-class mining:
+class mining(psql):
     def __init__(self) -> None:
-        self.host_name = env('HOSTNAME')
+        psql.__init__(self)
+        # self.host_name = env('HOSTNAME')
     
     def start_work(self):
         try:
-            cmd = psql().cmd
-            data = cmd('''select * from worker
-                       where working = false limit 1;''')
-            
+            self.exec('''select id from unit
+                       where state = 'idle' limit 1;''')
+            data = self.fetch_dict()
             # if no worker are available then break
             if not data:
                 raise Exception('no worker are Free')
             
             # change worker status on True
-            for worker in data.values():
-                id = worker['id']
-                cmd(f'''update worker set working = true
-                        where id = {id} returning working;''')
-                self.worker_ids.append(id) # add worker to pull
+            id = data[0]['id']
+            self.exec(f'''update unit set state = 'mining'
+                        where id = {id} returning state;''')
                 
             
             good = True    
             while good:
-                sleep(5)
+                sleep(1)
                 drop = self.gen_drop()
+                print(drop)
                 for item in drop:
-                    res = cmd(f'''update items
+                    res = self.exec(f'''update items
                     set amount = amount + {drop[item]}
-                    where name = '{item}' returning amount;''')
+                    where name = '{item}';''')
                     if not res:
                         good = False; continue
 
@@ -48,17 +47,11 @@ class mining:
                 
         except (Exception) as error:
             print(error.args)
-        finally:
-            for id in self.worker_ids:
-                result = cmd(f'''update worker
-                        set working = false
-                        where id = {id}
-                        returning working;''')
         
     def gen_drop(self):
-        drop =  {'Rock': randint(1,2) if randint(1, 10) > 5 else None,
-                'Energy rock': 1 if randint(1, 10) > 8 else None,
-                'Iron ore': 1 if randint(1, 100) > 95 else None}
+        drop =  {'rock': randint(1,2) if randint(1, 10) > 5 else None,
+                'energy_rock': 1 if randint(1, 10) > 8 else None,
+                'iron_ore': 1 if randint(1, 100) > 95 else None}
 
         drop = {n: drop[n] for n in drop if drop[n] is not None}
         return drop
