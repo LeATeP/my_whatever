53e91e440ec47a0f5fb13f7d44464aba247db076
leatep <leateps@gmail.com>
2022-02-21 07:23:15 +0400

    update vm setup

--- .install_scripts/install_vm_setup.sh
+++ .install_scripts/install_vm_setup.sh
@@ -9,3 +9,10 @@ sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://pack
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 sudo apt-get update && apt-get install -y kubelet kubeadm kubectl
+
+mkdir code
+(cd code && git clone https://github.com/LeATeP/my_whatever)
+
+cp /home/code/.bashrc ~/
+cp /home/code/.bash-aliases ~/
+cp /home/code/.vimrc ~/
