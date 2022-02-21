376be2fc0a18b48c104f79f354c4cc115876bc43
leatep <leateps@gmail.com>
2022-02-21 06:52:26 +0400

    add install script for vm

---
+++ .install_scripts/install_init_ubuntu_setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# 1. installing go, python/pip, vscode, google-chrome, data grip, lolcat, tweaks
+# 1.1 virtualbox, kind/k8s
+# 2. changing power-saving settings 
+# 3. set up bashrc, aliases, scripts
+# 4. setting up git, github ssh keys, 
+# 5. git, pulling main repos
+# 6. docker.. getting docker backup's, pull images: [ubuntu, python, golang]
